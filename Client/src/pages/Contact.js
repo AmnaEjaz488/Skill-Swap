4ce17094e02dc583eafde125f382dc52c1836a2f
amna <amna.ejaz488@gmail.com>
2025-04-29 19:23:45 -0400

    crated footer component

---
+++ Client/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="contact-page">
+      <h1>Contact Us</h1>
+      <p>Email: support@skillswap.com</p>
+      <p>Phone: #1234567891</p>
+    </div>
+  );
+};
+
+export default Contact;
