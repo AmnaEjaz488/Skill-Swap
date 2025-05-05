46ebd773a037b331d65106036c9e8a077958b832
amna <amna.ejaz488@gmail.com>
2025-05-04 21:51:24 -0400

    seed.js updated

--- Server-backend/seeds.js
+++ Server-backend/seeds.js
@@ -6,7 +6,7 @@ import Event from './models/Event.js';
 
 async function seed() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser:    true,
       useUnifiedTopology: true,
     });
